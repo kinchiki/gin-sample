37a3a2ceb090d7baab17d275a4cfc75c30a97f0a
Akito Naito <naito.akito@gmail.com>
2022-07-30 10:45:37 +0900

    created template dir and index.html

--- main.go
+++ main.go
@@ -9,17 +9,19 @@ func main() {
 	engine := gin.Default()
 	ua := ""
 
+	engine.LoadHTMLGlob("templates/*")
 	engine.Use(func(c *gin.Context) {
 		ua = c.GetHeader("User-Agent")
 		c.Next()
 	})
 
 	engine.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H {
+		c.HTML(http.StatusOK, "index.html", gin.H {
 			"message": "Hello World!",
-			"User-Agent": ua,
+			"userAgent": ua,
 		})
 	})
+
 	engine.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H {
 			"message": "pong",
