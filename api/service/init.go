cca3efea6048fec7f80f5e9e0f3db307cfd7f3f6
Akito Naito <naito.akito@gmail.com>
2022-08-06 01:02:45 +0900

    update info connecting mysql

--- api/service/init.go
+++ api/service/init.go
@@ -12,7 +12,7 @@ var DbEngine *xorm.Engine
 
 func init() {
 	driverName := "mysql"
-	DsName := "root:root@(192.168.99.100:3306)/gin?charset=utf8"
+	DsName := "root:root@([127.0.0.1]:3306)/gin_sample?charset=utf8mb4"
 	err := errors.New("")
 	DbEngine, err = xorm.NewEngine(driverName, DsName)
 	if err != nil && err.Error() != "" {
